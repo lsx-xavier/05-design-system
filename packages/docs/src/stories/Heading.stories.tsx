2234bce89aa93583e2ee232636edd77aad987760
Lucas Xavier <ls.xxavier@gmail.com>
2023-11-20 19:50:05 -0300

    ci: add turboRepo cache on deploy build

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Heading, HeadingProps } from '@ignite-ui/react'
+import { Heading, HeadingProps } from '@xavier-ignite-ui/react'
 import {
   Controls,
   Description,
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -7,7 +7,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@xavier-ignite-ui/tokens'
 
 export const {
   styled,
